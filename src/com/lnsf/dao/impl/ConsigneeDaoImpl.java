3e12f68a005cee4d4811e5fc7c2bbb578eea238d
SherlockLBS <1611597717@qq.com>
2019-08-01 16:30:51 +0800

    2019/08/01 16:30

---
+++ src/com/lnsf/dao/impl/ConsigneeDaoImpl.java
@@ -0,0 +1,70 @@
+package com.lnsf.dao.impl;
+
+import java.util.List;
+
+import com.lnsf.bean.ConsigneeBean;
+import com.lnsf.dao.ConsigneeDao;
+
+public class ConsigneeDaoImpl implements ConsigneeDao {
+
+	@Override
+	public int insertRecord(ConsigneeBean record) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int insertSelective(ConsigneeBean record) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int deleteById(Integer conId) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int updateByIdSelective(ConsigneeBean record) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int updateById(ConsigneeBean record) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int countRecord() {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int countSelective(ConsigneeBean record) {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public int maxId() {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public ConsigneeBean selectById(Integer conId) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public List selectAll() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
